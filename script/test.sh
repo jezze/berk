91204a04cca41c74c5d25e79030faa0a7ef987b3
Jens Nyberg <jens.nyberg@gmail.com>
2025-06-19 23:28:44 +0200

    More

--- script/test.sh
+++ script/test.sh
@@ -9,21 +9,32 @@ rm -rf ${WD}/tmp
 mkdir ${WD}/tmp
 cd ${WD}/tmp
 
+echo -n "Basic... "
 berk | grep -q "Usage: berk"
 berk log 2>&1 | grep -q "berk: Could not find '.berk' directory."
 berk init | grep -q "Initialized berk in '.berk'."
 berk log 2>&1 | grep -q "berk: Unable to open state."
+echo "PASSED"
+
+echo -n "Add local... "
 berk add -t local test | grep -q "Remote 'test' added."
+echo "PASSED"
+
+echo -n "Synchronous exec... "
 berk exec -n test "uptime" > /dev/null
 berk log | grep -q "id="
 berk log | wc | tr -s ' ' | grep -q " 5 9 144"
 berk log HEAD | grep -q "total=1"
 berk log HEAD 0 | grep -q "days"
+echo "PASSED"
+
+echo -n "Asynchronous exec... "
 berk exec test "uptime" > /dev/null
-sleep 1
+sleep 2
 berk log | grep -q "id="
 berk log HEAD | grep -q "run="
 berk log HEAD 0 | grep -q "days"
+echo "PASSED"
 
 cd ${WD}
 rm -rf ${WD}/tmp
