4e77ca2f7814098a376cedf7b1c98669b62d8768
Jens Nyberg <jens.nyberg@gmail.com>
2025-06-23 20:10:35 +0200

    more

--- script/test.sh
+++ script/test.sh
@@ -4,8 +4,10 @@ set -e
 
 WD="$(pwd)"
 BERK="${WD}/berk"
+VERSION="0.0.1"
 REMOTE1="myhost1"
 REMOTE2="myhost2"
+TESTDATA="testdata123testdata456"
 
 rm -rf ${WD}/tmp
 mkdir ${WD}/tmp
@@ -13,7 +15,7 @@ cd ${WD}/tmp
 
 echo -n "Uninitialized... "
 ${BERK} | grep -q "Usage: berk"
-${BERK} version | grep -q "berk version 0.0.1"
+${BERK} version | grep -q "berk version ${VERSION}"
 for cmd in "add config exec list log remove send shell stop wait"
 do
     ${BERK} ${cmd} 2>&1 | grep -q "berk: Could not find '.berk' directory."
@@ -25,35 +27,37 @@ ${BERK} init | grep -q "Initialized berk in '.berk'."
 ${BERK} log 2>&1 | grep -q "berk: Unable to open log."
 echo "PASSED"
 
-echo -n "Add remote... "
+echo -n "Add remotes... "
 ${BERK} add -t "local" "${REMOTE1}" | grep -q "Remote '${REMOTE1}' added."
 ${BERK} add -t "local" "${REMOTE2}" | grep -q "Remote '${REMOTE2}' added."
+${BERK} list | grep -q "${REMOTE1}"
+${BERK} list | grep -q "${REMOTE2}"
 echo "PASSED"
 
 echo -n "Send (single host, single file)... "
-echo "testdata" > file1.txt
+echo "${TESTDATA}" > file1.txt
 ${BERK} send "${REMOTE1}" "file1.txt" "file2.txt" > /dev/null
-cat file2.txt | grep -q "testdata"
+cat file2.txt | grep -q "${TESTDATA}"
 echo "PASSED"
 
 echo -n "Exec (synchronous, single host, status passed)... "
-${BERK} exec -n "${REMOTE1}" "echo test123" > /dev/null
+${BERK} exec -n "${REMOTE1}" "echo ${TESTDATA}" > /dev/null
 ${BERK} log HEAD | wc -l | grep -q "5"
 ${BERK} log HEAD | grep -q "id=.* datetime=.*"
 ${BERK} log HEAD | grep -q "total=1 complete=1 aborted=0 passed=1 failed=0"
 ${BERK} log HEAD | grep -q "    run=0 remote=${REMOTE1} status=passed"
-${BERK} log HEAD 0 | grep -q "test123"
+${BERK} log HEAD 0 | grep -q "${TESTDATA}"
 echo "PASSED"
 
 echo -n "Exec (synchronous, multiple hosts, status passed)... "
-${BERK} exec -n "${REMOTE1} ${REMOTE2}" "echo test123" > /dev/null
+${BERK} exec -n "${REMOTE1} ${REMOTE2}" "echo ${TESTDATA}" > /dev/null
 ${BERK} log HEAD | wc -l | grep -q "6"
 ${BERK} log HEAD | grep -q "id=.* datetime=.*"
 ${BERK} log HEAD | grep -q "total=2 complete=2 aborted=0 passed=2 failed=0"
 ${BERK} log HEAD | grep -q "    run=0 remote=${REMOTE1} status=passed"
 ${BERK} log HEAD | grep -q "    run=1 remote=${REMOTE2} status=passed"
-${BERK} log HEAD 0 | grep -q "test123"
-${BERK} log HEAD 1 | grep -q "test123"
+${BERK} log HEAD 0 | grep -q "${TESTDATA}"
+${BERK} log HEAD 1 | grep -q "${TESTDATA}"
 echo "PASSED"
 
 echo -n "Exec (synchronous, single host, status failed)... "
@@ -77,25 +81,25 @@ ${BERK} log -e HEAD 1 | grep -q "incorrectcommand: command not found"
 echo "PASSED"
 
 echo -n "Exec (asynchronous, single host, status passed)... "
-${BERK} exec "${REMOTE1}" "echo test123" > /dev/null
+${BERK} exec "${REMOTE1}" "echo ${TESTDATA}" > /dev/null
 ${BERK} wait HEAD
 ${BERK} log HEAD | wc -l | grep -q "5"
 ${BERK} log HEAD | grep -q "id=.* datetime=.*"
 ${BERK} log HEAD | grep -q "total=1 complete=1 aborted=0 passed=1 failed=0"
 ${BERK} log HEAD | grep -q "    run=0 remote=${REMOTE1} status=passed"
-${BERK} log HEAD 0 | grep -q "test123"
+${BERK} log HEAD 0 | grep -q "${TESTDATA}"
 echo "PASSED"
 
 echo -n "Exec (asynchronous, multiple hosts, status passed)... "
-#${BERK} exec "${REMOTE1} ${REMOTE2}" "echo test123" > /dev/null
+#${BERK} exec "${REMOTE1} ${REMOTE2}" "echo ${TESTDATA}" > /dev/null
 #${BERK} wait HEAD
 #${BERK} log HEAD | wc -l | grep -q "6"
 #${BERK} log HEAD | grep -q "id=.* datetime=.*"
 #${BERK} log HEAD | grep -q "total=2 complete=2 aborted=0 passed=2 failed=0"
 #${BERK} log HEAD | grep -q "    run=0 remote=${REMOTE1} status=passed"
 #${BERK} log HEAD | grep -q "    run=1 remote=${REMOTE2} status=passed"
-#${BERK} log HEAD 0 | grep -q "test123"
-#${BERK} log HEAD 1 | grep -q "test123"
+#${BERK} log HEAD 0 | grep -q "${TESTDATA}"
+#${BERK} log HEAD 1 | grep -q "${TESTDATA}"
 echo "PASSED"
 
 echo -n "Exec (asynchronous, single host, status failed)... "
