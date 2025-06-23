e28ee3a0f91e07a2025337ec0c84d69dbfab281e
Jens Nyberg <jens.nyberg@gmail.com>
2025-06-23 19:11:21 +0200

    more

--- script/test.sh
+++ script/test.sh
@@ -21,23 +21,23 @@ ${BERK} log 2>&1 | grep -q "berk: Unable to open log."
 echo "PASSED"
 
 echo -n "Local machine... "
-${BERK} add -t local ${REMOTE1} | grep -q "Remote '${REMOTE1}' added."
+${BERK} add -t "local" "${REMOTE1}" | grep -q "Remote '${REMOTE1}' added."
 echo "PASSED"
 
 echo -n "Synchronous exec... "
-${BERK} exec -n "${REMOTE1}" "uptime" > /dev/null
+${BERK} exec -n "${REMOTE1}" "echo test123" > /dev/null
 ${BERK} log | grep -q "id="
 ${BERK} log | wc -l | grep -q "5"
 ${BERK} log HEAD | grep -q "total=1"
-${BERK} log HEAD 0 | grep -q "load average"
+${BERK} log HEAD 0 | grep -q "test123"
 echo "PASSED"
 
 echo -n "Asynchronous exec... "
-${BERK} exec "${REMOTE1}" "uptime" > /dev/null
+${BERK} exec "${REMOTE1}" "echo test123" > /dev/null
 ${BERK} wait HEAD
 ${BERK} log | grep -q "id="
 ${BERK} log HEAD | grep -q "run="
-${BERK} log HEAD 0 | grep -q "load average"
+${BERK} log HEAD 0 | grep -q "test123"
 echo "PASSED"
 
 cd ${WD}
